3d4dde59149be6c8d4815091c1e676f0aa2c1481
jennyZam <91443477+jennyZam@users.noreply.github.com>
2024-04-15 23:37:21 -0500

    añadiendo pages administrador, usuario, nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { ServiciosComponent } from './servicios/servicios.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
+import { AdministradorComponent } from './administrador/administrador.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 let routes: Routes = [
   { path: '', component: DesignComponent},
@@ -12,7 +15,10 @@ let routes: Routes = [
   { path: 'servicios', component: ServiciosComponent},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
-  { path: 'contactanos', component: ContactanosComponent}
+  { path: 'contactanos', component: ContactanosComponent},
+  { path: 'administrador', component: AdministradorComponent},
+  { path: 'nosotros', component: NosotrosComponent},
+  { path: 'usuario', component: UsuarioComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ServiciosComponent } from './servicios/servicios.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
+import { AdministradorComponent } from './administrador/administrador.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { ContactanosComponent } from './contactanos/contactanos.component';
     ServiciosComponent,
     LoginComponent,
     SignupComponent,
-    ContactanosComponent
+    ContactanosComponent,
+    AdministradorComponent,
+    NosotrosComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
